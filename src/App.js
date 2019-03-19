d21682f4183d5d2fef8a5d2e7536e6bf5b059dfa
Adam Love <adam.love127@gmail.com>
2019-03-19 16:10:11 +1100

    Rename css files to scss and add font

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello, World</h1>
+      <div>
+        <h1>Hello, World</h1>
+        <div className="testFont">
+          Hello
+          <h1>Hello, World</h1>
+        </div>
+      </div>
     );
   }
 }
