f4b13d4c0973b386264edf1bad4a26ca7752501e
Adam Love <adam.love127@gmail.com>
2019-03-20 13:21:32 +1100

    Add ability to dismiss mobile nav drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.scss';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import PropTypes from 'prop-types';
 
-const layout = ({ children }) => (
-  <Aux>
-    <div>
-      <Toolbar />
-      <SideDrawer />
-      Toolbar SideDrawer Backdrop
-    </div>
-    <main className={styles.Layout__content}>{children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: true
+    };
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { showSideDrawer } = this.state;
+
+    return (
+      <Aux>
+        <div>
+          <Toolbar />
+          <SideDrawer
+            open={showSideDrawer}
+            closed={this.sideDrawerClosedHandler}
+          />
+          Toolbar SideDrawer Backdrop
+        </div>
+        <main className={styles.Layout__content}>{children}</main>
+      </Aux>
+    );
+  }
+}
+
+Layout.propTypes = {
+  children: PropTypes.node
+};
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,33 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.scss';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+import PropTypes from 'prop-types';
 
-const sideDrawer = () => {
+const sideDrawer = ({ open, closed }) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={open} clicked={closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
+};
+
+sideDrawer.propTypes = {
+  closed: PropTypes.func,
+  open: PropTypes.bool
 };
 
 export default sideDrawer;
