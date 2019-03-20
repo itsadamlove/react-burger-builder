4d782e7041216f1e0923e982138e27091a408513
Adam Love <adam.love127@gmail.com>
2019-03-20 13:47:40 +1100

    Improve performance by adding lifecycle hook to order summary
    Only Renender order summary if the modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,51 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import PropTypes from 'prop-types';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ({
-  ingredients,
-  purchaseCancelled,
-  purchaseContinued,
-  price
-}) => {
-  const ingredientSummary2 = Object.keys(ingredients)
-    .filter(igKey => {
-      return ingredients[igKey] > 0;
-    })
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-          {ingredients[igKey]}
-        </li>
-      );
-    });
+class OrderSummary extends Component {
+  // This is pretty neat, we modified modal to have a lifecycle hook
+  // so that we dont have to rerender the summary unless the modal
+  // is shown
+  componentDidUpdate() {
+    console.log('[OrderSummary] will update');
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary2}</ul>
-      <p>
-        <strong>Total Price: ${price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={purchaseCancelled} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={purchaseContinued} btnType="Success">
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+  render() {
+    const {
+      ingredients,
+      purchaseCancelled,
+      purchaseContinued,
+      price
+    } = this.props;
 
-orderSummary.propTypes = {
+    const ingredientSummary2 = Object.keys(ingredients)
+      .filter(igKey => {
+        return ingredients[igKey] > 0;
+      })
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+            {ingredients[igKey]}
+          </li>
+        );
+      });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary2}</ul>
+        <p>
+          <strong>Total Price: ${price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={purchaseCancelled} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={purchaseContinued} btnType="Success">
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+OrderSummary.propTypes = {
   ingredients: PropTypes.object.isRequired,
   purchaseCancelled: PropTypes.func.isRequired,
   purchaseContinued: PropTypes.func.isRequired,
   price: PropTypes.number
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Modal.module.scss';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 import PropTypes from 'prop-types';
 
-const modal = ({ children, show, modalClosed }) => (
-  <Aux>
-    <Backdrop show={show} clicked={modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: show ? 'translateY(0)' : 'translatey(-100vh)',
-        opacity: show ? '1' : '0'
-      }}
-    >
-      {children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  componentDidUpdate() {
+    console.log('[Modal] will update');
+  }
+
+  render() {
+    const { children, show, modalClosed } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translatey(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+        >
+          {children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+Modal.propTypes = {
   children: PropTypes.node,
-  show: PropTypes.bool
+  show: PropTypes.bool,
+  modalClosed: PropTypes.func
 };
 
-export default modal;
+export default Modal;
