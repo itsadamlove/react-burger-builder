3323dcc5c2d3a409c1103a91ddf1e720e3308d2f
Adam Love <adam.love127@gmail.com>
2019-03-20 12:15:50 +1100

    Add logo to navigation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+import styles from './Logo.module.scss';
+
+const logo = () => (
+  <div className={styles.Logo}>
+    <img src={burgerLogo} alt="burger logo" />
+  </div>
+);
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Toolbar.module.scss';
+import Logo from '../../Logo/Logo';
 
 const toolbar = () => (
   <header className={styles.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
