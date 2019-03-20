f0929742ecf3003572dad815d079e46f1fdd68dd
Adam Love <adam.love127@gmail.com>
2019-03-20 11:26:30 +1100

    Create default toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
-import styles from './Layout.module.scss'
+import Aux from '../../hoc/Aux';
+import styles from './Layout.module.scss';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
-const layout = ({children}) => (
+const layout = ({ children }) => (
   <Aux>
     <div>
-      Toolbar
-      SideDrawer
-      Backdrop
+      <Toolbar />
+      Toolbar SideDrawer Backdrop
     </div>
-    <main className={styles.Layout__content}>
-      {children}
-    </main>
+    <main className={styles.Layout__content}>{children}</main>
   </Aux>
 );
 
