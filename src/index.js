da9c52bf58499a2c6110df937b006d75b98a654e
Adam Love <adam.love127@gmail.com>
2019-04-01 10:08:17 +1100

    Add redux dev tools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,21 @@ import App from './App';
 import reducer from './store/reducer';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state');
+      return result;
+    };
+  };
+};
 
 const app = (
   <Provider store={store}>
